a3101bcfab37b9d912cd416db2c05e058cd31ef4
quytr <quy.truong310@gmail.com>
2022-04-10 20:40:47 -0400

    "Coding Exercise 35 - 36"

---
+++ src/academy/codingexercise35/Cylinder.java
@@ -0,0 +1,24 @@
+package academy.codingexercise35;
+
+public class Cylinder extends Circle {
+    private double height; // instance variable
+
+    //Create Constructor, initialization of fields
+    public Cylinder(double radius, double height) {
+        super(radius);
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    //Methods
+    public double getVolume() {
+        return getArea() * height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
